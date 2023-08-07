9505e363d495d0e09758b83e3c950a4f7606a13d
Farkhod Kodirov <farxod94@gmail.com>
2023-08-07 15:59:45 +0500

    feat: add db config, connect and user registration

--- authController.js
+++ authController.js
@@ -1,7 +1,27 @@
+const bcrypt = require("bcryptjs");
+const connection = require("./dbConfig");
+const User = require("./models/User");
 class authController {
   async registration(req, res) {
+    const { name, password, email } = req.body;
     try {
-    } catch (error) {}
+      const candidate = await User.findOne({ email });
+      if (candidate) {
+        return res
+          .status(400)
+          .json({ message: "A user with the same email already exists" });
+      }
+      const saltRounds = 8;
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const newUser = await User.create({
+        name: name,
+        email: email,
+        password: hashedPassword,
+      });
+      res.status(200).json({ message: "User registered successfully" });
+    } catch (error) {
+      res.status(400).json({ error: "Registeration error" });
+    }
   }
   async login(req, res) {
     try {
---
+++ models/User.js
@@ -0,0 +1,48 @@
+const { DataTypes, Model } = require("sequelize");
+const sequelize = require("../dbConfig");
+
+class User extends Model {}
+
+User.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    registrationDate: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    lastLoginDate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    status: {
+      type: DataTypes.ENUM("active", "blocked", "deleted"),
+      defaultValue: "active",
+    },
+  },
+  {
+    sequelize,
+    modelName: "User",
+    tableName: "users",
+    timestamps: false,
+  }
+);
+
+module.exports = User;
